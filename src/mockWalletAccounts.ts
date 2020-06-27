18f5664d84da8644e10e786ec3089c56b701a100
Gabriel Gil <gabrielbarbosagil@gmail.com>
2020-06-26 23:20:24 -0300

    Update mockWalletAccounts.ts

--- src/mockWalletAccounts.ts
+++ src/mockWalletAccounts.ts
@@ -10,16 +10,16 @@ export const mockWalletAccounts: Account[] = [
   ),
   new Account(
     '~src/assets/images/ovalGreen.png',
-    'My First Account',
-    24985,
-    'AN8iLVt18CKoATdexztCQj923hw5gkc41A',
+    'My Second Account',
+    38211,
+    '8tYaX4j3cTjjIAjbOjunNnPIds1Hgnnfce',
     '#1085C4'
   ),
   new Account(
     '~src/assets/images/ovalPurple.png',
-    'My First Account',
-    24985,
-    'AN8iLVt18CKoATdexztCQj923hw5gkc41A',
+    'My Third Account',
+    88206,
+    'PbiI8k3RxiQm44xOKJ1Zs9mpBDrH0C0Mre',
     '#5525C5'
   ),
 ]
