1061436eb51eec7ffc1c22a8dba86f7b5cd46c67
RyanLopes <ryan.lopes@simpli.com.br>
2020-07-21 14:14:43 -0300

    NW-166 Scan QR code screen

--- src/scenes/TestPage/QRCodeScanTest.tsx
+++ src/scenes/TestPage/QRCodeScanTest.tsx
@@ -1,41 +1,63 @@
-import React from 'react';
-
-import {StyleSheet, Button, View, SafeAreaView, Text, Alert, Image} from 'react-native';
-import {TextView} from '~src/styles/styled-components'
-import ScreenLayout from "~src/components/layout/ScreenLayout";
-import Constants from 'expo-constants';
-import { BlurView } from 'expo-blur';
-import {alignItems, alignSelf, bottom} from "styled-system";
+import {BarCodeScanner} from 'expo-barcode-scanner'
+import React, {useState, useEffect} from 'react'
+import {Text, View, StyleSheet, Button, Image} from 'react-native'
+import {ImageView} from "~src/styles/styled-components";
 
 
-const QrCodePage = () => (
-    <ScreenLayout>
-        <View>
-            <Image
-                style={styles.logo}
-                source={require('~src/assets/images/qr-code-frame.png')}
+
+
+export default function QRCodeScanTest() {
+
+    const [hasPermission, setHasPermission] = useState(true)
+    const [scanned, setScanned] = useState(false)
+
+    useEffect(() => {
+        ;(async () => {
+            const {status} = await BarCodeScanner.requestPermissionsAsync()
+            setHasPermission(status === 'granted')
+        })()
+    }, [])
+
+    // @ts-ignore
+    const handleBarCodeScanned = ({type, data}) => {
+        if (!scanned) {
+            setScanned(true)
+            // eslint-disable-next-line no-undef
+            alert(`Bar code with type ${type} and data ${data} has been scanned!`)
+        }
+    }
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>
+    } else if (hasPermission === false) {
+        return <Text>No access to camera</Text>
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+            }}
+        >
+            <ImageView position='absolute' width='52px'
+                       source={require('~src/assets/images/qr-code-frame.png')}
             />
-        </View>
-        <View style = {styles.container}>
-            <Button
-                color="#CDCDCD"
-                title="Cancel"
-                onPress={() => Alert.alert('Cancel')}
+            <BarCodeScanner
+                onBarCodeScanned={handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+
+
             />
+
+
+            {scanned && (
+                <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+            )}
+
         </View>
-    </ScreenLayout>
-);
 
-const styles = StyleSheet.create({
-    container: {
-        paddingTop: 187,
-    },
-    colour:{
-        color:"green"
-    },
-    logo: {
-        alignSelf:"center",
-    },
-});
 
-export default QrCodePage;
+    )
+}
