52969bfcb8d716527207d12cd3501b9a1d7bef60
Felipe Gibran Eleuterio Toledo <ft.gibran@gmail.com>
2020-07-28 11:45:19 -0300

    NW-228 Integrate Account Card with its Balance, getting it from the WS

--- src/app/Sync.ts
+++ src/app/Sync.ts
@@ -1,6 +1,9 @@
+import {Account} from '~src/models/redux/Account'
 import {App} from '~src/models/redux/App'
 import {Settings} from '~src/models/redux/Settings'
+import {Wallet} from '~src/models/redux/Wallet'
 import {RootStore} from '~src/store/RootStore'
+import {Exchange} from '~src/types/exchange'
 
 export type SyncResult = {
   settings: Settings
@@ -8,11 +11,23 @@ export type SyncResult = {
 
 export abstract class Sync {
   static async init(dispatch: AsyncDispatch<any>): Promise<SyncResult> {
-    const settings = await dispatch(RootStore.settings.actions.syncSettings())
+    const settings: Settings = await dispatch(
+      RootStore.settings.actions.syncSettings()
+    )
 
-    const exchange = await dispatch(RootStore.app.actions.syncExchange())
-    const wallets = await dispatch(RootStore.app.actions.syncWallets())
-    const accounts = await dispatch(RootStore.app.actions.syncAccounts())
+    const exchange: Exchange = await dispatch(
+      RootStore.app.actions.syncExchange()
+    )
+    const wallets: Wallet[] = await dispatch(
+      RootStore.app.actions.syncWallets()
+    )
+    const accounts: Account[] = await dispatch(
+      RootStore.app.actions.syncAccounts()
+    )
+
+    // Synchronize balance from WS
+    const promises = accounts.map((it) => it.populateBalanceHistory())
+    await Promise.all(promises)
 
     return {settings, exchange, wallets, accounts}
   }
