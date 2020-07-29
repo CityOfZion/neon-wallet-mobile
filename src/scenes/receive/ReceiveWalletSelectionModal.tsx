0be92805ec4cdea21836ea611a2669e0e5c63c30
RyanLopes <ryan.lopes@simpli.com.br>
2020-07-29 11:13:06 -0300

    NW-168 Ajustes finos fluxo Receive

--- src/scenes/receive/ReceiveWalletSelectionModal.tsx
+++ src/scenes/receive/ReceiveWalletSelectionModal.tsx
@@ -31,7 +31,7 @@ const ReceiveWalletSelectionModal = (props: Props) => {
       controller={controller}
       fullSize={true}
       paddingTop={24}
-      paddingRight={0}
+      paddingRight={8}
       paddingLeft={0}
       title={Facade.t('modals.receive.title')}
       rightButton={CloseButton()}
