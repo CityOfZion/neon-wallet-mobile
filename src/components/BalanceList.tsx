71982af3c27c5ac19fb132ed7d8919cbc4e71b54
Ricardo Prado <ricardo.prado@simpli.com.br>
2020-08-27 15:03:21 -0300

    NW-341

--- src/components/BalanceList.tsx
+++ src/components/BalanceList.tsx
@@ -23,63 +23,98 @@ const BalanceList = (props: Props) => {
   delete innerProps.fromAccountView
   delete innerProps.account
 
-  const TableData = (props: {header: string; content: string}) => {
-    return (
-      <LinearLayout weight={1} orientation="verti" mt={5} mb={4}>
-        <TextView
-          mb="-6px"
-          color="text.2"
-          fontSize="lg"
-          allowFontScaling={true}
-          numberOfLines={1}
-          ellipsizeMode="tail"
-          mr={4}
-        >
-          {props.header}
-        </TextView>
-        <TextView
-          color="text.0"
-          fontSize="xl"
-          fontFamily="medium"
-          allowFontScaling={true}
-          adjustsFontSizeToFit={true}
-          numberOfLines={1}
-        >
-          {props.content}
-        </TextView>
-      </LinearLayout>
-    )
-  }
-
   const ViewBalanceItem = (props: {item: TokenAsset}) => {
     return (
-      <LinearLayout orientation="horiz" alignItems="center">
+      <LinearLayout
+        orientation="horiz"
+        alignItems="center"
+        alignContent={'center'}
+      >
         <ImageView
           mr={'8px'}
-          width={Facade.scale(32)}
-          height={Facade.scale(32)}
+          width={Facade.scale(24)}
+          height={Facade.scale(24)}
           resizeMode={'contain'}
           source={props.item.srcIcon}
+          alginSelf={'center'}
         />
 
-        <TableData header={props.item.name} content={props.item.symbol} />
-
-        <LinearLayout weight={1} ml={4}>
-          <TableData
-            header={Facade.t('components.balanceList.holdings')}
-            content={String(props.item.amount)}
-          />
+        <LinearLayout weight={1} orientation="verti" mt={5} mb={4}>
+          <TextView
+            color="text.0"
+            fontSize="xl"
+            fontFamily="medium"
+            allowFontScaling={true}
+            adjustsFontSizeToFit={true}
+            numberOfLines={1}
+            mb={'6px'}
+          >
+            {props.item.symbol}
+          </TextView>
         </LinearLayout>
 
         <LinearLayout weight={1} ml={4}>
-          <TableData
-            header={Facade.t('components.balanceList.value')}
-            content={Facade.filter.currency(
-              props.item.exchange(currency, exchange),
-              currency,
-              language
-            )}
-          />
+          <LinearLayout weight={1} orientation="verti" justifyContent={'center'}>
+            <TextView
+              mb="-6px"
+              color="text.2"
+              fontSize="sm"
+              allowFontScaling={true}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+              mr={4}
+            >
+              {Facade.t('components.balanceList.holdings')}
+            </TextView>
+            <TextView
+              mt={1}
+              color="text.2"
+              fontSize="sm"
+              fontFamily="medium"
+              allowFontScaling={true}
+              adjustsFontSizeToFit={true}
+              numberOfLines={1}
+            >
+              {Facade.t('components.balanceList.value')}
+            </TextView>
+          </LinearLayout>
+        </LinearLayout>
+        <LinearLayout weight={1} />
+        <LinearLayout weight={1} ml={4}>
+          <LinearLayout
+            weight={1}
+            orientation="verti"
+            mt={5}
+            mb={4}
+            alignItems={'flex-end'}
+          >
+            <TextView
+              mb="-6px"
+              color="text.0"
+              fontSize="md"
+              allowFontScaling={true}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+              textAlign={'right'}
+            >
+              {String(props.item.amount)}
+            </TextView>
+            <TextView
+              mt={1}
+              color="primary"
+              fontSize="md"
+              fontFamily="medium"
+              allowFontScaling={true}
+              adjustsFontSizeToFit={true}
+              numberOfLines={1}
+            >
+              {Facade.filter.currency(
+                props.item.exchange(currency, exchange),
+                currency,
+                language
+              )}
+            </TextView>
+          </LinearLayout>
         </LinearLayout>
       </LinearLayout>
     )
