ba895e2af5156c1844fb724a99844a63e1fbef10
Thais Bastos <thais.bastos@simpli.com.br>
2020-09-04 16:18:15 -0300

    NW-311 Screen - Import Key -after import key, the screen is going to "Screen welcome", but the correct screen is "Detail screen assets"

--- native_modules/neo-sdk-bindings/index.d.ts
+++ native_modules/neo-sdk-bindings/index.d.ts
@@ -1,7 +1,4 @@
-declare module "react-native-native-neo-bindings" {
-    const decryptNep2: (
-        key: string,
-        password: string,
-    ) => Promise<string>;
-    export default decryptNep2;
+declare module 'react-native-native-neo-bindings' {
+  const decryptNep2: (key: string, password: string) => Promise<string>
+  export default decryptNep2
 }
