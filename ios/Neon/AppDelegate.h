7474a00209d399c0c2f516aee60f9347ca202558
user925523 <user925523@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-10-21 15:38:56 -0400

    Final build

--- ios/Neon/AppDelegate.h
+++ ios/Neon/AppDelegate.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <EXUpdates/EXUpdatesAppController.h>
-#import <React/RCTBridgeDelegate.h>
+#					import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
 #import <UMCore/UMAppDelegateWrapper.h>
