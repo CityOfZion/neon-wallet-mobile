11777ea2dec41a3d7107dca65b905236ccaa3a41
Thais Bastos <thaiscrysbastos@gmail.com>
2021-01-21 15:57:36 -0300

    NW-636 IOS 6.6 QR send, watch or contact #562

--- src/scenes/HandleQRModal.tsx
+++ src/scenes/HandleQRModal.tsx
@@ -4,10 +4,7 @@ import {ImageSourcePropType, TouchableWithoutFeedback} from 'react-native'
 import {useSelector} from 'react-redux'
 
 import {Facade} from '~src/app/Facade'
-import SwiperPanel, {
-  SwiperController,
-  CloseButton,
-} from '~src/components/SwiperPanel'
+import SwiperPanel, {SwiperController} from '~src/components/SwiperPanel'
 import ThemedCloseButton from '~src/components/themed/ThemedCloseButton'
 import {ImageView, LinearLayout, TextView} from '~src/styles/styled-components'
 interface ListItem {
@@ -78,44 +75,54 @@ const HandleQRModal = (props: Props) => {
       draggable={false}
       paddingTop={0}
       paddingBottom={42}
-      rightButton={<CloseButton mr={'20px'} />}
-      onRightPress={props.controller.close}
+      noHeader={true}
+      solidColorBG={true}
     >
-      {items.map((item, index) => (
-        <TouchableWithoutFeedback key={index} onPress={item.onClick}>
-          <LinearLayout mx="16px">
-            <LinearLayout
-              orientation="horiz"
-              pb="18px"
-              pt="16px"
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <LinearLayout>
-                <TextView color={theme.colors.text[0]} fontSize={18}>
-                  {item.title}
-                </TextView>
-                <TextView
-                  color={theme.colors.text[6]}
-                  fontSize={14}
-                  fontFamily="medium"
+      <LinearLayout height={'60px'} />
+      <LinearLayout>
+        {items.map((item, index) => (
+          <>
+            <TouchableWithoutFeedback key={index} onPress={item.onClick}>
+              <LinearLayout mx="10px">
+                <LinearLayout
+                  orientation="horiz"
+                  pb="18px"
+                  pt="16px"
+                  alignItems="center"
+                  justifyContent="space-between"
                 >
-                  {item.subtitle}
-                </TextView>
+                  <LinearLayout>
+                    <TextView color={theme.colors.text[0]} fontSize={18}>
+                      {item.title}
+                    </TextView>
+                    <TextView
+                      color={theme.colors.text[6]}
+                      fontSize={16}
+                      fontFamily="medium"
+                    >
+                      {item.subtitle}
+                    </TextView>
+                  </LinearLayout>
+
+                  <ImageView width={36} height={36} source={item.source} />
+                </LinearLayout>
+
+                <LinearLayout height="1px" bg={theme.colors.background[5]} />
               </LinearLayout>
-
-              <ImageView
-                width={35}
-                height={35}
-                mr="13px"
-                source={item.source}
-              />
-            </LinearLayout>
-
-            <LinearLayout height="1px" bg={theme.colors.background[5]} />
-          </LinearLayout>
-        </TouchableWithoutFeedback>
-      ))}
+            </TouchableWithoutFeedback>
+          </>
+        ))}
+      </LinearLayout>
+      <LinearLayout
+        position={'absolute'}
+        right={Facade.scale(-18)}
+        top={Facade.scale(3)}
+      >
+        <ThemedCloseButton
+          iconSize={[18, 27]}
+          onPress={props.controller.close}
+        />
+      </LinearLayout>
     </SwiperPanel>
   )
 }
