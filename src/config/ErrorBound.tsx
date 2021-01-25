ffdf12318ee2c8cccb4fe07fa1f33fed399d3463
Kenedy Ribeiro <helplace2019@gmail.com>
2021-01-25 14:48:33 -0300

    NW-676 corretion typescript errorbound

--- src/config/ErrorBound.tsx
+++ src/config/ErrorBound.tsx
@@ -2,28 +2,27 @@ import React from 'react'
 import { Text, ScrollView } from 'react-native'
 
 interface IErrorBoundary {
-
+    errorMsg: string,
+    infoMsg: string,
+    hasError: boolean
 }
 
-class ErrorBoundary extends React.Component {
+class ErrorBoundary extends React.Component<{}, IErrorBoundary> {
     constructor(props: IErrorBoundary) {
         super(props);
         this.state = { hasError: false, errorMsg: '', infoMsg: '' };
     }
-    //@ts-ignore
-    componentDidCatch(error, info) {
+   
+    componentDidCatch(error: any, info: any) {
         this.setState({ hasError: true, errorMsg: JSON.stringify(error), infoMsg: JSON.stringify(info) });
     }
     render() {
-        //@ts-ignore
         if (this.state.hasError) {
-            //@ts-ignore
-           
             return (
                 <ScrollView style={{ alignSelf: 'center', marginTop: 100, marginHorizontal: 15 }}>
                     <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#000' }}>Error occurred.</Text>
-                    <Text style={{marginTop: 30}}>Error => {this.state.errorMsg}</Text>
-                    <Text>Info => {this.state.infoMsg}</Text>
+                    <Text style={{ marginTop: 30 }}>Error --- {this.state.errorMsg}</Text>
+                    <Text>Info --- {this.state.infoMsg}</Text>
                 </ScrollView>
             )
         }
