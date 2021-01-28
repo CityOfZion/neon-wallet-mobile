e84058e82ffecd1d2437a385c2c2113aff3f2f8e
Thais Bastos <thaiscrysbastos@gmail.com>
2021-01-28 18:42:26 -0300

    NW-628 IOS 4.5 Edit Account overlay #554

--- src/components/input/InputClearButton.tsx
+++ src/components/input/InputClearButton.tsx
@@ -11,9 +11,10 @@ export const InputClearButton = (props: {
     <TouchableWithoutFeedback onPress={props.onPress} disabled={!props.value}>
       <ImageView
         opacity={props.value ? 1 : 0}
-        width={15}
-        height={15}
-        mb="-6px"
+        width={14}
+        height={14}
+        mt="5px"
+        mr="4px"
         resizeMode="contain"
         source={require('~/src/assets/images/icon-cancel-grey.png')}
       />
