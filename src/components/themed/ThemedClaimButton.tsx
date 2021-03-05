4ae51cff07df57887fbfcc60adad0571d1696e85
Thais Bastos <thaiscrysbastos@gmail.com>
2021-03-05 18:26:46 -0300

    Screen account detail - Buttons are incorrect "Send/receive/claim Gas #807

--- src/components/themed/ThemedClaimButton.tsx
+++ src/components/themed/ThemedClaimButton.tsx
@@ -3,12 +3,18 @@ import React from 'react'
 import {
   TouchableWithoutFeedback,
   StyleSheet,
-  Image,
   GestureResponderEvent,
   View,
 } from 'react-native'
+
+import {
+  RelativeLayout,
+  LinearLayout,
+  LinearGradientLayout,
+} from '~/src/styles/styled-components'
 interface Props {
   onPress: (evt: GestureResponderEvent) => void
+  isClaimAvailable: boolean
   children?: any
 }
 
@@ -20,7 +26,6 @@ const ThemedClaimButton: React.FC<Props> = (props) => {
       width: 209,
       height: 45,
       borderRadius: 7,
-      backgroundColor: '#364046dd',
       shadowColor: '#464d53',
       shadowOffset: {
         width: 12,
@@ -28,13 +33,54 @@ const ThemedClaimButton: React.FC<Props> = (props) => {
       },
       shadowRadius: 7,
       elevation: 30,
-      borderWidth: 1,
-      borderColor: '#ffffff22',
     },
   })
   return (
     <TouchableWithoutFeedback onPress={props.onPress}>
-      <View style={styles.dropShadow}>{props.children}</View>
+      <View style={styles.dropShadow}>
+        <RelativeLayout
+          width={'100%'}
+          height={'100%'}
+          alignItems="center"
+          justifyContent="center"
+          borderRadius={7}
+        >
+          <LinearLayout
+            width="100%"
+            height="100%"
+            borderRadius={7}
+            overflow="hidden"
+            position="absolute"
+          >
+            <LinearGradientLayout
+              width="100%"
+              height="100%"
+              colors={['#464c52', '#1c2329']}
+              end={[0.1, 0.8]}
+            />
+          </LinearLayout>
+          <LinearGradientLayout
+            width="98%"
+            height="98%"
+            borderRadius={7}
+            overflow="hidden"
+            justifyContent={'center'}
+            colors={
+              props.isClaimAvailable
+                ? ['#41515b', '#28333b']
+                : ['#313e46', '#20292f']
+            }
+          >
+            <LinearLayout
+              width="100%"
+              orientation="horiz"
+              justifyContent={'center'}
+            >
+              {props.children}
+            </LinearLayout>
+          </LinearGradientLayout>
+        </RelativeLayout>
+      </View>
     </TouchableWithoutFeedback>
   )
 }
@@ -42,6 +88,7 @@ const ThemedClaimButton: React.FC<Props> = (props) => {
 ThemedClaimButton.propTypes = {
   onPress: PropTypes.func.isRequired,
   children: PropTypes.any.isRequired,
+  isClaimAvailable: PropTypes.bool.isRequired,
 }
 
 export {ThemedClaimButton}
--- src/components/themed/ThemedSendButton.tsx
+++ src/components/themed/ThemedSendButton.tsx
@@ -8,6 +8,12 @@ import {
   View,
 } from 'react-native'
 
+import {Facade} from '~/src/app/Facade'
+import {
+  LinearGradientLayout,
+  LinearLayout,
+  RelativeLayout,
+} from '~/src/styles/styled-components'
 import {Account} from '~src/models/redux/Account'
 interface Props {
   account?: Account
@@ -25,7 +31,6 @@ const ThemedSendButton: React.FC<Props> = (props) => {
       width: 71,
       height: 45,
       borderRadius: 7,
-      backgroundColor: '#364046dd',
       shadowColor: '#464d53',
       shadowOffset: {
         width: 12,
@@ -33,26 +38,65 @@ const ThemedSendButton: React.FC<Props> = (props) => {
       },
       shadowRadius: 7,
       elevation: 30,
-      borderWidth: 1,
-      borderColor: '#ffffff22',
     },
   })
   return (
     <TouchableWithoutFeedback onPress={props.onPress}>
       <View style={styles.dropShadow}>
-        {isNotEmpty && !isWatchAccount ? (
-          <Image
-            width={13}
-            height={16}
-            source={require('~src/assets/images/arrow-up-green.png')}
-          />
-        ) : (
-          <Image
-            width={13}
-            height={16}
-            source={require('~src/assets/images/arrow-gray.png')}
-          />
-        )}
+        <RelativeLayout
+          width={'100%'}
+          height={'100%'}
+          alignItems="center"
+          justifyContent="center"
+          borderRadius={7}
+        >
+          <LinearLayout
+            width="100%"
+            height="100%"
+            borderRadius={7}
+            overflow="hidden"
+            position="absolute"
+          >
+            <LinearGradientLayout
+              width="100%"
+              height="100%"
+              colors={['#464c52', '#1c2329']}
+              end={[0.3, 1]}
+            />
+          </LinearLayout>
+          <LinearGradientLayout
+            width="96%"
+            height="96%"
+            borderRadius={7}
+            overflow="hidden"
+            justifyContent={'center'}
+            colors={
+              isNotEmpty && !isWatchAccount
+                ? ['#41515b', '#28333b']
+                : ['#313e46', '#20292f']
+            }
+          >
+            <LinearLayout
+              width="100%"
+              orientation="horiz"
+              justifyContent={'center'}
+            >
+              {isNotEmpty && !isWatchAccount ? (
+                <Image
+                  width={13}
+                  height={16}
+                  source={require('~src/assets/images/arrow-up-green.png')}
+                />
+              ) : (
+                <Image
+                  width={13}
+                  height={16}
+                  source={require('~src/assets/images/arrow-gray.png')}
+                />
+              )}
+            </LinearLayout>
+          </LinearGradientLayout>
+        </RelativeLayout>
       </View>
     </TouchableWithoutFeedback>
   )
