326d5e6868c48e6b3a06c4b93aee5a04e8f478e8
Kenedy Ribeiro <helplace2019@gmail.com>
2022-01-19 19:09:58 -0300

    remove console.log

--- src/store/senderTransaction/SendTransactionReducer.ts
+++ src/store/senderTransaction/SendTransactionReducer.ts
@@ -59,7 +59,6 @@ export class SendTransactionReducer extends ReducerWrapper<
             sendTx
           )
         } catch (error) {
-          console.log(error)
           throw new Error('Transaction has failed')
         }
       }
