b8ebccd888e44f8f5001900f774a8ede559cc6e8
Kenedy Ribeiro <helplace2019@gmail.com>
2022-02-07 14:00:43 -0300

    release ios 07022022

--- ios/Neon/AppDelegate.m
+++ ios/Neon/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import <UMCore/UMModuleRegistry.h>
 #import <UMReactNativeAdapter/UMNativeModulesProxy.h>
-#import <UMReactNativeAdapRCTLinkingManagerter/UMModuleRegistryAdapter.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
 #import <EXSplashScreen/EXSplashScreenService.h>
 #import <UMCore/UMModuleRegistryProvider.h>
 #if DEBUG
