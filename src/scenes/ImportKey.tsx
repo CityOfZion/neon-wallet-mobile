6abbc6e1e1317e8e0cf9190ff748ebdc18cb6451
Kenedy Ribeiro <helplace2019@gmail.com>
2022-03-04 15:03:49 -0300

    import mnemonic doesn't work #1489

--- src/scenes/ImportKey.tsx
+++ src/scenes/ImportKey.tsx
@@ -82,30 +82,35 @@ const ImportKey = (props: ImportKeyProps) => {
         wif: string
         derivationIndex: number
       }[] = []
-      for (const blockchainName of blockchainList) {
-        stop = false
-        index = 0
-        while (!stop && isConnected) {
-          const {wif, address} = blockchainServices[
-            blockchainName
-          ].generateAccount(mnemonic, index)
-          if (!accounts.find((account) => account.address === address)) {
-            const req = blockchainServices[blockchainName].provider
-            const {totalEntries} = await req.getAddressAbstracts(
-              address,
-              tokens,
-              1
-            )
-            if ((totalEntries && totalEntries > 0) || index === 0) {
-              accountsInfo.push({address, wif, derivationIndex: index})
-            } else {
-              stop = true
+      try {
+        for (const blockchainName of blockchainList) {
+          stop = false
+          index = 0
+          while (!stop && isConnected) {
+            const {wif, address} = blockchainServices[
+              blockchainName
+            ].generateAccount(mnemonic, index)
+            if (!accounts.find((account) => account.address === address)) {
+              await UtilsHelper.sleep(200)
+              const req = blockchainServices[blockchainName].provider
+              const {totalEntries} = await req.getAddressAbstracts(
+                address,
+                tokens,
+                1
+              )
+              if ((totalEntries && totalEntries > 0) || index === 0) {
+                accountsInfo.push({address, wif, derivationIndex: index})
+              } else {
+                stop = true
+              }
             }
+            index++
           }
-          index++
+          allAccountsInfo.set(blockchainName, accountsInfo)
+          accountsInfo = []
         }
-        allAccountsInfo.set(blockchainName, accountsInfo)
-        accountsInfo = []
+      } catch (error) {
+        throw error
       }
 
       if (!isConnected) {
