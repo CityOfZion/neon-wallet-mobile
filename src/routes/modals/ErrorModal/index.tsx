c2b8cf207573dfbee511134adb0abffc109c115f
hotequil <hotequil@protonmail.com>
2026-03-24 10:17:10 -0300

    CU-86agda4rt-NWM - Fix done button on dApp modals

--- src/routes/modals/ErrorModal/index.tsx
+++ src/routes/modals/ErrorModal/index.tsx
@@ -3,8 +3,6 @@ import { Text, View } from 'react-native'
 import { TwButton } from '@/components/TwButton'
 import { TwSeparator } from '@/components/TwSeparator'
 
-import { StyleHelper } from '@/helpers/StyleHelper'
-
 import { useModalErase } from '@/hooks/useModalErase'
 
 import { TwModalLayout } from '@/layouts/TwModalLayout'
@@ -15,7 +13,7 @@ import MdOutlineCancel from '@/assets/images/md-outline-cancel.svg'
 import type { TRootStackScreenProps } from '@/types/stacks'
 
 export const ErrorModal = ({ route }: TRootStackScreenProps<'ErrorModal'>) => {
-  const { buttonProps, content, title, onClose } = route.params
+  const { buttonLabel, content, title, onClose } = route.params
   const { handleErase } = useModalErase()
 
   const handleClose = () => {
@@ -40,16 +38,11 @@ export const ErrorModal = ({ route }: TRootStackScreenProps<'ErrorModal'>) => {
         )}
       </View>
 
-      {buttonProps && (
+      {buttonLabel && (
         <View className="w-full gap-7">
           <TwSeparator />
 
-          <TwButton
-            onPress={handleClose}
-            {...buttonProps}
-            className={StyleHelper.mergeStyles('w-full', buttonProps?.className)}
-            variant="contained-light"
-          />
+          <TwButton label={buttonLabel} onPress={handleClose} className="w-full" variant="contained-light" />
         </View>
       )}
     </TwModalLayout>
