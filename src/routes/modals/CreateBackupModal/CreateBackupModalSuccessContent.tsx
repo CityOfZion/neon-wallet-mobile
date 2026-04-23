cb97957f4de85f099360ce26a33ed52be7355b1a
hotequil <hotequil@protonmail.com>
2026-04-23 13:20:12 -0300

    CU-86ah218g4-NWM - Remove extension on save files

--- src/routes/modals/CreateBackupModal/CreateBackupModalSuccessContent.tsx
+++ src/routes/modals/CreateBackupModal/CreateBackupModalSuccessContent.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
-import * as Print from 'expo-print'
 import { useTranslation } from 'react-i18next'
 import { Text, View } from 'react-native'
 import QrCode from 'react-native-qrcode-svg'
 
 import { TwButton } from '@/components/TwButton'
+
+import { DateHelper } from '@/helpers/DateHelper'
+import { ToastHelper } from '@/helpers/ToastHelper'
+
+import { useFileSystem } from '@/hooks/useFileSystem'
 
 import TbDeviceFloppy from '@/assets/images/tb-device-floppy.svg'
 
@@ -15,13 +19,17 @@ type TProps = {
 
 export const CreateBackupModalSuccessContent = ({ password }: TProps) => {
   const { t } = useTranslation('modals', { keyPrefix: 'createBackup' })
+  const { t: tCommonGeneral } = useTranslation('common', { keyPrefix: 'general' })
+  const { writeFile } = useFileSystem()
 
   const [qrCodeText, setQrCodeText] = useState('')
 
-  const handleSavePassword = () => {
+  const handleSavePassword = async () => {
     if (!qrCodeText) return
 
-    Print.printAsync({ html: `<img aria-hidden="true" src="data:image/jpeg;base64,${qrCodeText}" alt="" />` })
+    await writeFile(`NEON-qr-code-${DateHelper.getNowUnix()}`, qrCodeText, 'image/png')
+
+    ToastHelper.success({ message: tCommonGeneral('savedSuccessfully') })
   }
 
   return (
