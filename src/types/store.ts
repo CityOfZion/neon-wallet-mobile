6f2b5b55dcf5166f38943753f47a2ee18968305a
Thais Bastos <thais@coz.io>
2026-07-15 19:20:43 -0300

    CU-86ajbdmck - NWM - Remove Neo Legacy migration notification after July 8

--- src/types/store.ts
+++ src/types/store.ts
@@ -134,12 +134,6 @@ export type TNotificationNavigateActionBNeoShutdownPayload = {
   blockchain: TBlockchainServiceKey
 }
 
-export type TNotificationNavigateActionNeoLegacyMigrationPayload = {
-  to: 'neo-legacy-migration'
-  address: string
-  blockchain: TBlockchainServiceKey
-}
-
 export type TNotificationNavigateAction = {
   type: 'navigate'
   payload:
@@ -163,7 +157,6 @@ export type TNotificationNavigateAction = {
         to: 'backup-wallet'
       }
     | TNotificationNavigateActionBNeoShutdownPayload
-    | TNotificationNavigateActionNeoLegacyMigrationPayload
 }
 
 export type TNotificationAction = TNotificationNavigateAction
